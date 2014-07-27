900f017af39532f58ff3e574d2053da3a9dd5325
whole.heaven@gmail.com <whole.heaven@gmail.com@e01e27f6-5406-b7a7-9e73-f95e1cecfe5b>
2014-07-27 13:55:33 +0000

    Access denied not admin

---
+++ src/org/dbwiki/web/ui/printer/server/DatabaseAccessDeniedPrinter.java
@@ -0,0 +1,24 @@
+package org.dbwiki.web.ui.printer.server;
+
+
+import org.dbwiki.exception.WikiException;
+import org.dbwiki.web.html.HtmlLinePrinter;
+import org.dbwiki.web.request.parameter.RequestParameterAction;
+import org.dbwiki.web.ui.CSS;
+import org.dbwiki.web.ui.HtmlContentGenerator;
+import org.dbwiki.web.ui.printer.HtmlContentPrinter;
+
+public class DatabaseAccessDeniedPrinter extends HtmlContentGenerator implements HtmlContentPrinter{
+
+	@Override
+	public void print(HtmlLinePrinter printer) throws WikiException {
+		// TODO Auto-generated method stub
+		printer.paragraph("Denied.", CSS.CSSHeadline);
+		printer.openFORM("", "POST", "/");
+		printer.paragraph("Sorry, you are not an administrator. :(", CSS.CSSPageContent);
+		printer.addREALBUTTON("submit",
+				"action", RequestParameterAction.ActionCancel, "<img src=\"/pictures/button_cancel.gif\">");
+		printer.closeFORM();
+	}
+
+}
