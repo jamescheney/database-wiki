6eb947965957cb5076ce71ea30b32c2bdb10b02b
whole.heaven@gmail.com <whole.heaven@gmail.com@e01e27f6-5406-b7a7-9e73-f95e1cecfe5b>
2014-07-27 13:24:14 +0000

    No access permission page

---
+++ src/org/dbwiki/web/security/NoAccessPermissionPage.java
@@ -0,0 +1,15 @@
+package org.dbwiki.web.security;
+
+import org.dbwiki.web.html.HtmlPage;
+
+public class NoAccessPermissionPage extends HtmlPage{
+	
+	/*
+	 * Constructors
+	 */
+	
+	public NoAccessPermissionPage() {
+		this.add("<html><head><title>Database Wiki - No Access Permission</title></head><body><h1>No Access Permission<h2><p>You don't have permissions, please contact the administrator!</p><p><a href='/?action=cancel'>Back to Server Home.</a></p>");
+	}
+}
+
